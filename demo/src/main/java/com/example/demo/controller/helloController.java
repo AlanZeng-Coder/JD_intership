d91795b699dfd63e86b817b8c2dfa9203af9b6c9
Alanz <zienzeng0510@gmail.com>
2024-01-26 09:52:30 -0800

    Done

---
+++ demo/src/main/java/com/example/demo/controller/helloController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class helloController {
+    @RequestMapping("/testHello")
+    public String hello(){
+        return "Hello Spring Boot!";
+    }
+}
