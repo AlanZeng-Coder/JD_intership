8a0390febf8090a39a0b412dd6096a0193a06a91
Alanz <zienzeng0510@gmail.com>
2024-02-01 03:43:34 -0800

    add a domo1 with mysql

---
+++ demo/demo_with_MySQL/src/main/java/com/example/demo_with_mysql/DemoWithMySqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_with_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithMySqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWithMySqlApplication.class, args);
+    }
+
+}
