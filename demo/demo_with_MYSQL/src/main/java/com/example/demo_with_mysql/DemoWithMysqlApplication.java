2770e4d30d20b8fabeafc27538701d4f567abea4
Alanz <zienzeng0510@gmail.com>
2024-02-01 03:49:20 -0800

    create new demo with MYSQL

---
+++ demo/demo_with_MYSQL/src/main/java/com/example/demo_with_mysql/DemoWithMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_with_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWithMysqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWithMysqlApplication.class, args);
+    }
+
+}
---
+++ demo/demo_with_MYSQL/src/test/java/com/example/demo_with_mysql/DemoWithMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_with_mysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWithMysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
