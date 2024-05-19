83e520ec3baa9ebd5cde64a4647239229d78381f
Alanz <zienzeng0510@gmail.com>
2024-05-19 10:17:12 -0700

    change to new file

--- demo/src/main/java/com/example/demo/controller/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.controller;// Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
