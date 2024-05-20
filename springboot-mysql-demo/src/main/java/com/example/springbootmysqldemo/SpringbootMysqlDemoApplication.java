de554a92aa9a3825a1292250fc820a427e87cade
Alanz <zienzeng0510@gmail.com>
2024-05-19 20:45:32 -0700

    upload spring boot

--- springboot-mysql-demo/src/main/java/com/example/springbootmysqldemo/SpringbootMysqlDemoApplication.java
+++ springboot-mysql-demo/src/main/java/com/example/springbootmysqldemo/SpringbootMysqlDemoApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootMysqlDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SpringbootMysqlDemoApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlDemoApplication.class, args);
+		System.out.print("Hello world");
+	}
 
 }
--- springboot-mysql-demo/src/test/java/com/example/springbootmysqldemo/SpringbootMysqlDemoApplicationTests.java
+++ springboot-mysql-demo/src/test/java/com/example/springbootmysqldemo/SpringbootMysqlDemoApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SpringbootMysqlDemoApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
