214fd10319e2c773f84a4ee6369137aef4e183f8
Alanz <zienzeng0510@gmail.com>
2024-05-20 22:11:07 -0700

    finish maven and spring boot with mybatis and Mysql.

---
+++ springboot-mybatis-demo/src/main/java/com/example/springbootmybatisdemo/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.example.springbootmybatisdemo.controller;
+
+import com.example.springbootmybatisdemo.model.User;
+import com.example.springbootmybatisdemo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userService.findAll();
+    }
+}
---
+++ springboot-mybatis-demo/src/main/java/com/example/springbootmybatisdemo/service/UserService.java
@@ -0,0 +1,22 @@
+package com.example.springbootmybatisdemo.service;
+
+import com.example.springbootmybatisdemo.mapper.UserMapper;
+import com.example.springbootmybatisdemo.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    public List<User> findAll() {
+        return userMapper.findAll();
+    }
+}
